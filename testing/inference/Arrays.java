41dd8720a6bd12e14a475d133a7d315000690514
Jeff Luo <j36luo@uwaterloo.ca>
2018-03-07 01:04:56 -0500

    add tests that don't work and need future work

---
+++ testing/inference/Arrays.java
@@ -0,0 +1,24 @@
+import units.qual.UnknownUnits;
+import units.qual.m;
+import units.qual.UnitsBottom;
+import units.qual.Dimensionless;
+import units.UnitsTools;
+// @skip-test
+// need to be able to set default array component qualifier, and fix some crazy bug
+
+@UnknownUnits
+class Arrays {
+    Arrays() {
+        @m int[] arrayOfMeters1 = null;
+
+        @m int[] arrayOfMeters2 = new int @Dimensionless [5];
+
+        @m int[] arrayOfMeters3 = new @m int @Dimensionless [5];
+
+        @m int[] arrayOfMeters4 = new int @Dimensionless [] {UnitsTools.m, UnitsTools.m};
+
+        @m int[] arrayOfMeters5 = new @m int @Dimensionless [] {UnitsTools.m, UnitsTools.m};
+
+//         @m int[] arrayOfMeters6 = {UnitsTools.m, UnitsTools.m};
+    }
+}
---
+++ testing/inference/TryWithResources.java
@@ -0,0 +1,16 @@
+import units.qual.*;
+import units.UnitsTools;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+// @skip-test
+// unable to insert inferred annotations for resource variables in try blocks
+// TODO: try variable defaulting?
+
+class TryWithResources {
+    static String readFirstLineFromFile(String path) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            return br.readLine();
+        }
+    }
+}

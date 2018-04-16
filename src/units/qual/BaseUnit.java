8fae908159caa019ee94e8727c252274312fd921
Jeff Luo <j36luo@uwaterloo.ca>
2018-04-16 13:45:12 -0400

    update qual and stub

--- src/units/qual/BaseUnit.java
+++ src/units/qual/BaseUnit.java
@@ -11,6 +11,7 @@ public @interface BaseUnit {
     // value of p in 10 ^ p as a prefix, for kilo p = 3, for milli p = -3
     int prefix() default 0;
 
+    // TODO: replace with Class<? extends Annotation> unit();
     String unit() default "none";
 
     int exponent() default 0;

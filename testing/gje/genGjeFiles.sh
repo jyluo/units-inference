206073df4dff845817c83eced85b9da0f2570d34
Jeff Luo <j36luo@uwaterloo.ca>
2018-11-18 14:14:09 -0500

    add gje test file generation script

---
+++ testing/gje/genGjeFiles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+../../run-units-infer.sh gauss ExactConstrained.java
+mv gjeConstraints_m.gje sampleFiles/exact.gje
+
+../../run-units-infer.sh gauss OverSpecified.java
+mv gjeConstraints_m.gje sampleFiles/overspec.gje
+
+../../run-units-infer.sh gauss UnderConstrained.java
+mv gjeConstraints_m.gje sampleFiles/under.gje
+
+../../run-units-infer.sh gauss Unsat.java
+mv gjeConstraints_m.gje sampleFiles/unsat.gje
+
+../../run-units-infer.sh gauss ExactConstrained.java OverSpecified.java UnderConstrained.java
+mv gjeConstraints_m.gje sampleFiles/mixSat.gje
+
+../../run-units-infer.sh gauss *.java
+mv gjeConstraints_m.gje sampleFiles/mixUnsat.gje
+
+rm *.gje
+rm *.txt
+rm *.class

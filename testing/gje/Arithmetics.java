3595f8faf49d094f02ffdee90c078991a376e3d7
Jeff Luo <j36luo@uwaterloo.ca>
2018-11-18 15:18:18 -0500

    split up underconstrained tests

---
+++ testing/gje/Arithmetics.java
@@ -0,0 +1,16 @@
+import units.UnitsTools;
+import units.qual.*;
+
+class Arithmetics {
+    void arithmetics() {
+        @m int a = 5 + 6;
+
+        @s int s = 5 - 6;
+
+        int m = 5 * 6;
+
+        int d = 5 / 6;
+
+        int mo = 5 % 6;
+    }
+}
--- testing/gje/UnderConstrained.java
+++ testing/gje/UnderConstrained.java
@@ -6,16 +6,4 @@ class UnderConstrained {
         int x = z;
         int y = x;
     }
-
-    void arithmetics() {
-        @m int a = 5 + 6;
-
-        @s int s = 5 - 6;
-
-        int m = 5 * 6;
-
-        int d = 5 / 6;
-
-        int mo = 5 % 6;
-    }
 }

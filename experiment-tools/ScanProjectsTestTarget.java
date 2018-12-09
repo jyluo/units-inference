e0ad8c1a15c0b4f7ae91eeba80c2cbd08e1dd71d
Jeff Luo <j36luo@uwaterloo.ca>
2018-12-09 00:38:40 -0500

    fix GJF targets

--- experiment-tools/ScanProjectsTestTarget.java
+++ experiment-tools/ScanProjectsTestTarget.java
@@ -4,19 +4,20 @@ import java.time.LocalDate;
 import static javafx.scene;
 import static java.time.LocalDate;
 
+class Test {
+    void calls() {
+        System.currentTimeMillis();
 
-System.currentTimeMillis();
+        Math.sin(5 + 6);
+        Math.cos(5 + 6);
+        Math.tan(5 + 6);
 
+        Math.asin(5 + 6);
+        Math.acos(5 + 6);
+        Math.atan(5 + 6);
 
-Math.sin(5 + 6);
-Math.cos(5 + 6);
-Math.tan(5 + 6);
-
-Math.asin(5 + 6);
-Math.acos(5 + 6);
-Math.atan(5 + 6);
-
-Math.sinh(5 + 6);
-Math.cosh(5 + 6);
-Math.tanh(5 + 6);
-
+        Math.sinh(5 + 6);
+        Math.cosh(5 + 6);
+        Math.tanh(5 + 6);
+    }
+}

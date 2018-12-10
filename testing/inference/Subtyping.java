b066f1138c37fe7a1f98defa8d88ea01529cc0e5
Jeff Luo <j36luo@uwaterloo.ca>
2018-12-10 02:22:13 -0500

    update subtyping tests

--- testing/inference/Subtyping.java
+++ testing/inference/Subtyping.java
@@ -37,4 +37,72 @@ class SubtypingTest {
     void flexibleSub(int x) {
         @UnknownUnits int y = x;
     }
+
+    class DirectSubtypeCases {
+        @UnknownUnits int top;
+        @Dimensionless int dim;
+        @m int m;
+        @s int s;
+        @UnitsBottom int bot;
+
+        void callTop(@UnknownUnits int top) {
+        }
+
+        void callDim(@Dimensionless int dim) {
+        }
+
+        void callM(@m int m) {
+        }
+
+        void callS(@s int s) {
+        }
+
+        void callBot(@UnitsBottom int bot) {
+        }
+
+        void sat() {
+            int inferTop = top;
+            int inferDim = dim;
+            int inferM = m;
+            int inferS = s;
+            int inferBot = bot;
+
+            callTop(inferTop);
+            callTop(inferDim);
+            callTop(inferM);
+            callTop(inferS);
+            callTop(inferBot);
+
+            callDim(inferDim);
+            callDim(inferBot);
+
+            callM(inferM);
+            callM(inferBot);
+
+            callS(inferS);
+            callS(inferBot);
+
+            callBot(inferBot);
+        }
+
+        // void unsat() {
+        //     int inferTop = top;
+        //     int inferDim = dim;
+        //     int inferM = m;
+        //     int inferS = s;
+
+        //     // callDim(inferTop);
+        //     // callDim(inferM);
+        //     // callDim(inferS);
+
+        //     // callM(inferTop);
+        //     // callM(inferDim);
+        //     // callM(inferS);
+
+        //     // callBot(inferTop);
+        //     // callBot(inferDim);
+        //     // callBot(inferM);
+        //     // callBot(inferS);
+        // }
+    }
 }

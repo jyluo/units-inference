d5fdcb7d3a6a1d37ffe538ecf278ad088e99db9d
Jeff Luo <j36luo@uwaterloo.ca>
2018-12-29 00:17:52 -0500

    update flow refinement test

--- testing/typecheck/FlowRefinement.java
+++ testing/typecheck/FlowRefinement.java
@@ -4,17 +4,18 @@ import units.qual.*;
 class FlowRefinement {
 
     void methodLocalFlowSensitiveRefinement() {
-        @deg double d = 45.0 * UnitsTools.deg;
+        @deg double degrees = 45.0 * UnitsTools.deg;
 
-        double temp = Math.toRadians(d);  // temp is refined to @rad during the assignment
-
+        // temp is refined to @rad (radians) during the assignment
+        double temp = Math.toRadians(degrees);
         System.out.println("angle in radians: " + temp);
 
-        temp = Math.sin(temp);  // temp is refined to @Dimensionless during the assignment
-
+        // temp is refined to @Dimensionless during the assignment
+        temp = Math.sin(temp);
         System.out.println("sine of angle: " + temp);
 
+        // Math.cos() requires the argument to be in the unit of radians
         // :: error: (argument.type.incompatible)
-        temp = Math.cos(temp);  // Math.cos() requires the argument to be in the unit of radians
+        temp = Math.cos(temp);
     }
 }

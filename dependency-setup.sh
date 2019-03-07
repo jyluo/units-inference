bf79a3404eb3a0090718495676fc44c21eb02d45
Jeff Luo <j36luo@uwaterloo.ca>
2019-03-07 16:18:33 -0500

    set upstream branch

--- dependency-setup.sh
+++ dependency-setup.sh
@@ -18,7 +18,7 @@ echo "------ Downloading everthing from REPO_SITE: $REPO_SITE ------"
 if [ -d $JSR308/checker-framework-inference ] ; then
     (cd $JSR308/checker-framework-inference && git pull)
 else
-    BRANCH=master
+    BRANCH=upstream_update_mar_5_2019
     (cd $JSR308 && git clone -b $BRANCH --depth 1 https://github.com/"$REPO_SITE"/checker-framework-inference.git)
 fi
 

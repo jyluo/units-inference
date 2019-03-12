aa86fa1ebed33006d112662dfda501a72d356e71
Jeff Luo <j36luo@uwaterloo.ca>
2019-03-11 21:41:16 -0400

    update UnitsTools

--- src/units/UnitsTools.java
+++ src/units/UnitsTools.java
@@ -1,33 +1,33 @@
 package units;
 
-import units.notusedquals.g;
-import units.notusedquals.kg;
 import units.qual.*;
 
-@SuppressWarnings("units")
+/** Utility methods to generate annotated types and to convert between them. */
+@SuppressWarnings({"units", "checkstyle:constantname"})
 public class UnitsTools {
-    // Static Constants
+    public static final @A int A = 1;
+    public static final @C int C = 1;
+    public static final @cd int cd = 1;
+    public static final @day int day = 1;
+    public static final @deg double deg = 1;
     public static final @g int g = 1;
+    public static final @h int h = 1;
+    public static final @K int K = 1;
     public static final @kg int kg = 1;
-
-    public static final @m int m = 1;
     public static final @km int km = 1;
-
-    public static final @s int s = 1;
+    public static final @km2 int km2 = 1;
+    public static final @kmPERh int kmPERh = 1;
+    public static final @m int m = 1;
+    public static final @m2 int m2 = 1;
+    public static final @min int min = 1;
+    public static final @mm int mm = 1;
+    public static final @mm2 int mm2 = 1;
+    public static final @mol int mol = 1;
+    public static final @mPERs int mPERs = 1;
+    public static final @mPERs2 int mPERs2 = 1;
     public static final @ms int ms = 1;
-    public static final @us int us = 1;
     public static final @ns int ns = 1;
-
-    public static final @rad int rad = 1;
-    public static final @deg int deg = 1;
-
-    // Testing use only
-    public static final @UnknownUnits int top = 1;
-    public static final @UnitsBottom int bottom = 1;
-    public static final @Dimensionless int dimensionless = 1;
-
-    // Conversion Functions
-    public static final @ms int secondsToMilliSeconds(@s int seconds) {
-        return seconds * 1000;
-    }
+    public static final @rad double rad = 1;
+    public static final @s int s = 1;
+    public static final @us int us = 1;
 }

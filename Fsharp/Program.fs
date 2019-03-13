d1ea1d21589cdce98294cb245ba446488d90925f
Jeff Luo <j36luo@uwaterloo.ca>
2019-03-13 04:33:34 -0400

    Update Program.fs

--- Fsharp/Program.fs
+++ Fsharp/Program.fs
@@ -1,30 +1,21 @@
-open UnitsTestMultiModule.HelperLib
+open UnitsTypeSystem
 
-[<Measure>] type km
-[<Measure>] type h
-[<Measure>] type s
+module Main =
+    // calling a function from another module, the parameter types are assumed to be dimensionless integers
+    let s1 = HelperLib.calcSpeed(HelperLib.e, HelperLib.e)  // ok if local call in HelperLib is commented, compile time error if not commented
+    let speedError = HelperLib.calcSpeed(HelperLib.d, HelperLib.t1) // compile time error if above is not commented
 
-// constants for testing type system
-let d = 30<km>
-let t1 = 10<h>
-let t2 = 10<s>
-let e = 10<1>   // same as dimensionless int by itself
+    // calling a function explicitly annotated as polymorphic is ok
+    let s1Poly = HelperLib.calcSpeedPoly(HelperLib.d, HelperLib.t1)  // ok
 
-// calling a function from another module, the parameter types are assumed to be dimensionless integers
-// let speedError = calcSpeed(d,t) // compile time error
-let s1 = calcSpeed(e,e)  // ok
+    // lets also define a local copy of both methods found in HelperLib
+    let localCalcSpeed (dist, time) = dist / time
+    let localCalcSpeedPoly (dist:int<_>, time:int<_>) = dist / time
 
-// calling a function explicitly annotated as polymorphic is ok
-let s1Poly = calcSpeedPoly(d,t1)  // ok
+    // calling the local unannotated copy is ok, F# type inference infers the units of the function parameters and returns
+    let s2 = localCalcSpeed(HelperLib.d, HelperLib.t1)
 
-// lets also define a local copy of both methods found in HelperLib
-let localCalcSpeed (dist, time) = dist / time
-let localCalcSpeedPoly (dist:int<_>, time:int<_>) = dist / time
+    let s2BadCall = localCalcSpeed(HelperLib.d, HelperLib.t2)   // error issued here, the type signature of localCalcSpeed is already unified iwth the call for variable s2, and cannot unify with this call
 
-// calling the local unannotated copy is ok, F# type inference infers the units of the function parameters and returns
-let s2 = localCalcSpeed(d, t1)
-
-let s2BadCall = localCalcSpeed(d, t2)   // error issued here, the type signature of localCalcSpeed is already unified iwth the call for variable s2, and cannot unify with this call
-
-// calling the local polymorphic annotated copy is also ok
-let lSpeedPoly = localCalcSpeedPoly(d, t1)
+    // calling the local polymorphic annotated copy is also ok
+    let lSpeedPoly = localCalcSpeedPoly(HelperLib.d, HelperLib.t1)

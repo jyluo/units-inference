1be7850a31d4d924e1b69e926300899eeefb348e
Jeff Luo <j36luo@uwaterloo.ca>
2020-08-07 13:16:03 -0400

    Fix variable names in StackFrame

--- coq-proof/StackFrame.v
+++ coq-proof/StackFrame.v
@@ -8,26 +8,26 @@ type Tv and labeled value l = Tl z. Formally v is mapped to a tuple, but for
 implementation convenience we model l explicitly as a term and use Coq's
 built-in pair data structure: F = v -> Tv, (Tl, n) .
 *)
-Definition StackFrameValue : Type := prod Unit Value.
+Definition StackFrameValue : Type := prod Unit LabeledLiteral.
 
 Definition StackFrameValue_beq ( fv1 fv2 : StackFrameValue ) : bool :=
   match fv1, fv2 with
-  | (u1, l1), (u2, l2) => if unit_beq u1 u2 then if Value_beq l1 l2 then true else false else false
+  | (u1, l1), (u2, l2) => if unit_beq u1 u2 then if LabeledLiteral_beq l1 l2 then true else false else false
   end.
 
 Definition StackFrame := Map ID StackFrameValue.
 
 Definition empty_heap := Empty_Map ID StackFrameValue.
-Definition StackFrame_Update (f : StackFrame) (v : ID) (Tv Tl : Unit) (z : nat) : StackFrame := Map_Add id_beq h v (Tv, Val Tl z).
-Definition StackFrame_Get (h : StackFrame) (v : ID) : option StackFrameValue := Map_Get id_beq h v.
-Definition StackFrame_Contains (h : StackFrame) (v : ID) : bool := Map_Contains id_beq h v.
-Definition StackFrame_IsSubMap (h1 h2 : StackFrame) : bool := Map_IsSubMap id_beq StackFrameValue_beq h1 h2.
+Definition StackFrame_Update (f : StackFrame) (v : ID) (Tv Tl : Unit) (z : nat) : StackFrame := Map_Add id_beq f v (Tv, Val Tl z).
+Definition StackFrame_Get (f : StackFrame) (v : ID) : option StackFrameValue := Map_Get id_beq f v.
+Definition StackFrame_Contains (f : StackFrame) (v : ID) : bool := Map_Contains id_beq f v.
+Definition StackFrame_IsSubMap (f1 f2 : StackFrame) : bool := Map_IsSubMap id_beq StackFrameValue_beq f1 f2.
 Hint Unfold StackFrame_Update : pUnitsHintDatabase.
 
 Theorem StackFrame_Get_Content_Eq :
-  forall (h : StackFrame) (v : ID) (l1 l2 : option StackFrameValue),
-  StackFrame_Get h v = l1 ->
-  StackFrame_Get h v = l2 ->
+  forall (f : StackFrame) (v : ID) (l1 l2 : option StackFrameValue),
+  StackFrame_Get f v = l1 ->
+  StackFrame_Get f v = l2 ->
   l1 = l2.
 Proof.
   unfold StackFrame_Get.
@@ -39,33 +39,33 @@ Qed.
 
 (* Function for looking up the static type of a variable.
 If v exists in F, it returns the type otherwise it returns None *)
-Definition FieldType (h : StackFrame) (v : ID) : option Unit :=
-  match StackFrame_Get h v with
+Definition VarType (f : StackFrame) (v : ID) : option Unit :=
+  match StackFrame_Get f v with
   | Some (pair u v) => Some u
   | None => None
   end.
 
-Theorem FieldType_Content_Eq:
-  forall (h : StackFrame) (v : ID) (T1 T2 : Unit),
-  FieldType h v = Some T1 ->
-  FieldType h v = Some T2 ->
+Theorem VarType_Content_Eq:
+  forall (f : StackFrame) (v : ID) (T1 T2 : Unit),
+  VarType f v = Some T1 ->
+  VarType f v = Some T2 ->
   T1 = T2.
 Proof.
   intros.
   rewrite -> H in H0. inversion H0. reflexivity.
 Qed.
 
-(* function for looking up the field value of a field *)
-Definition FieldValue (h : StackFrame) (v : ID) : option Value :=
-  match StackFrame_Get h v with
+(* function for looking up the labeled value of a variable *)
+Definition VarValue (f : StackFrame) (v : ID) : option LabeledLiteral :=
+  match StackFrame_Get f v with
   | Some (pair u v) => Some v
   | None => None
   end.
 
-Theorem FieldValue_Content_Eq:
-  forall (h : StackFrame) (v : ID) (l1 l2 : Value),
-  FieldValue h v = Some l1 ->
-  FieldValue h v = Some l2 ->
+Theorem VarValue_Content_Eq:
+  forall (f : StackFrame) (v : ID) (l1 l2 : LabeledLiteral),
+  VarValue f v = Some l1 ->
+  VarValue f v = Some l2 ->
   l1 = l2.
 Proof.
   intros.
@@ -73,77 +73,77 @@ Proof.
 Qed.
 
 Theorem StackFrame_Contains_Implies_Get :
-  forall (h : StackFrame) (v : ID),
-  StackFrame_Contains h v = true ->
-  exists (Tv Tl : Unit) (z : nat), StackFrame_Get h v = Some (Tv, Val Tl z).
+  forall (f : StackFrame) (v : ID),
+  StackFrame_Contains f v = true ->
+  exists (Tv Tl : Unit) (z : nat), StackFrame_Get f v = Some (Tv, Val Tl z).
 Proof.
   unfold StackFrame_Contains.
   unfold StackFrame_Get.
   intros.
   unfold Map_Contains in H.
-  destruct (Map_Get id_beq h v).
-    destruct h0. destruct v.
+  destruct (Map_Get id_beq f v).
+    destruct s. destruct l.
     exists u. exists u0. exists n. reflexivity.
     inversion H.
 Qed.
 
-Theorem StackFrame_Contains_Implies_FieldType :
-  forall (h : StackFrame) (v : ID),
-  StackFrame_Contains h v = true ->
-  exists (Tv : Unit), FieldType h v = Some Tv.
+Theorem StackFrame_Contains_Implies_VarType :
+  forall (f : StackFrame) (v : ID),
+  StackFrame_Contains f v = true ->
+  exists (Tv : Unit), VarType f v = Some Tv.
 Proof.
   intros.
-  unfold FieldType.
+  unfold VarType.
   apply StackFrame_Contains_Implies_Get in H.
   inversion H as [Tv]. inversion H0 as [Tl]. inversion H1 as [z].
   rewrite -> H2.
   exists Tv. reflexivity.
 Qed.
 
-Theorem StackFrame_Contains_Implies_FieldValue :
-  forall (h : StackFrame) (v : ID),
-  StackFrame_Contains h v = true ->
-  exists (Tl : Unit) (z : nat), FieldValue h v = Some (Val Tl z).
+Theorem StackFrame_Contains_Implies_VarValue :
+  forall (f : StackFrame) (v : ID),
+  StackFrame_Contains f v = true ->
+  exists (Tl : Unit) (z : nat), VarValue f v = Some (Val Tl z).
 Proof.
   intros.
-  unfold FieldValue.
+  unfold VarValue.
   apply StackFrame_Contains_Implies_Get in H.
   inversion H as [Tv]. inversion H0 as [Tl]. inversion H1 as [z].
   rewrite -> H2.
   exists Tl, z. reflexivity.
 Qed.
 
-Theorem StackFrame_Update_FieldType_Eq :
-  forall (h : StackFrame) (v : ID) (Tv Tl : Unit) (z : nat),
-  FieldType (StackFrame_Update h v Tv Tl z) v = Some Tv.
+Theorem StackFrame_Update_VarType_Eq :
+  forall (f : StackFrame) (v : ID) (Tv Tl : Unit) (z : nat),
+  VarType (StackFrame_Update f v Tv Tl z) v = Some Tv.
 Proof.
   intros.
-  unfold FieldType. unfold StackFrame_Update. unfold StackFrame_Get.
-  induction h; subst; simpl.
-  (* Case : h is empty *)
+  unfold VarType. unfold StackFrame_Update. unfold StackFrame_Get.
+  induction f; subst; simpl.
+  (* Case : f is empty *)
     rewrite -> id_beq_true. reflexivity.
-  (* Case : h has one or more elements *)
+  (* Case : f has one or more elements *)
     destruct a as [v' hv']. destruct hv' as [Tl' z'].
     destruct (id_eq_dec v v'); subst.
       rewrite -> id_beq_true. simpl. rewrite -> id_beq_true. reflexivity.
       rewrite -> id_beq_false. simpl.
         rewrite -> id_beq_false.
-          apply IHh.
+          apply IHf.
           apply n.
         apply n.
 Qed.
 
-Theorem StackFrame_Update_FieldType_Neq :
-  forall (h : StackFrame) (f1 f2 : ID) (Tv Tl : Unit) (z : nat),
+Theorem StackFrame_Update_VarType_Neq :
+  forall (f : StackFrame) (f1 f2 : ID) (Tv Tl : Unit) (z : nat),
   f2 <> f1 ->
-  FieldType (StackFrame_Update h f1 Tv Tl z) f2 = FieldType h f2.
+  VarType (StackFrame_Update f f1 Tv Tl z) f2 = VarType f f2.
 Proof.
   intros.
-  unfold FieldType. unfold StackFrame_Update. unfold StackFrame_Get.
-  induction h; subst; simpl.
-  (* Case : h is empty *)
+  unfold VarType. unfold StackFrame_Update. unfold StackFrame_Get.
+  induction f; subst; simpl.
+  (* Case : f is empty *)
     rewrite -> id_beq_false. reflexivity. apply H.
-  (* Case : h has one or more elements *)
+  (* Case : f has one or more elements *)
     destruct a as [v' hv']. destruct hv' as [Tl' z'].
     destruct (id_eq_dec f1 v'); subst.
       rewrite -> id_beq_true. simpl. rewrite -> id_beq_false.
@@ -156,43 +156,43 @@ Proof.
           rewrite -> id_beq_true. rewrite -> id_beq_true. reflexivity.
           rewrite -> id_beq_false.
             rewrite -> id_beq_false.
-              apply IHh.
+              apply IHf.
               apply n0.
             apply n0.
         apply n.
 Qed.
 
-Theorem StackFrame_Update_FieldValue_Eq :
-  forall (h : StackFrame) (v : ID) (Tv Tl : Unit) (z : nat),
-  FieldValue (StackFrame_Update h v Tv Tl z) v = Some (Val Tl z).
+Theorem StackFrame_Update_VarValue_Eq :
+  forall (f : StackFrame) (v : ID) (Tv Tl : Unit) (z : nat),
+  VarValue (StackFrame_Update f v Tv Tl z) v = Some (Val Tl z).
 Proof.
   intros.
-  unfold FieldValue. unfold StackFrame_Update. unfold StackFrame_Get.
-  induction h; subst; simpl.
-  (* Case : h is empty *)
+  unfold VarValue. unfold StackFrame_Update. unfold StackFrame_Get.
+  induction f; subst; simpl.
+  (* Case : f is empty *)
     rewrite -> id_beq_true. reflexivity.
-  (* Case : h has one or more elements *)
-    destruct a as [f' hv']. destruct hv' as [Tl' z'].
-    destruct (id_eq_dec f f'); subst.
+  (* Case : f has one or more elements *)
+    destruct a as [v' hv']. destruct hv' as [Tl' z'].
+    destruct (id_eq_dec v v'); subst.
       rewrite -> id_beq_true. simpl. rewrite -> id_beq_true. reflexivity.
       rewrite -> id_beq_false. simpl.
         rewrite -> id_beq_false.
-          apply IHh.
+          apply IHf.
           apply n.
         apply n.
 Qed.
 
-Theorem StackFrame_Update_FieldValue_Neq :
-  forall (h : StackFrame) (f1 f2 : ID) (Tv Tl : Unit) (z : nat),
+Theorem StackFrame_Update_VarValue_Neq :
+  forall (f : StackFrame) (f1 f2 : ID) (Tv Tl : Unit) (z : nat),
   f2 <> f1 ->
-  FieldValue (StackFrame_Update h f1 Tv Tl z) f2 = FieldValue h f2.
+  VarValue (StackFrame_Update f f1 Tv Tl z) f2 = VarValue f f2.
 Proof.
   intros.
-  unfold FieldValue. unfold StackFrame_Update. unfold StackFrame_Get.
-  induction h; subst; simpl.
-  (* Case : h is empty *)
+  unfold VarValue. unfold StackFrame_Update. unfold StackFrame_Get.
+  induction f; subst; simpl.
+  (* Case : f is empty *)
     rewrite -> id_beq_false. reflexivity. apply H.
-  (* Case : h has one or more elements *)
+  (* Case : f has one or more elements *)
     destruct a as [f' hv']. destruct hv' as [Tl' z'].
     destruct (id_eq_dec f1 f'); subst.
       rewrite -> id_beq_true. simpl. rewrite -> id_beq_false.
@@ -205,7 +205,7 @@ Proof.
           rewrite -> id_beq_true. rewrite -> id_beq_true. reflexivity.
           rewrite -> id_beq_false.
             rewrite -> id_beq_false.
-              apply IHh.
+              apply IHf.
               apply n0.
             apply n0.
         apply n.
